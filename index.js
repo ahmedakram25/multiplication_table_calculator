47cd8a78ae4fe8603a56c6cf819ad7c2ce1267b0
ahmedakram25 <200042659@must.edu.eg>
2025-09-18 23:31:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,481 @@
+const num1 = document.getElementById("num1")
+const num2 = document.getElementById("num2")
+const num3 = document.getElementById("num3")
+const num4 = document.getElementById("num4")
+const num5 = document.getElementById("num5")
+const num6 = document.getElementById("num6")
+const num7 = document.getElementById("num7")
+const num8 = document.getElementById("num8")
+const num9 = document.getElementById("num9")
+
+const plus = document.getElementById("plus")
+const minus = document.getElementById("minus")
+const mult = document.getElementById("mult")
+const divde = document.getElementById("divde")
+const rest = document.getElementById("rest")
+const ans = document.getElementById("ans")
+let x,y;
+
+
+    function n1(){
+        x = 1;
+mult.onclick = function(){
+        num1.onclick = function(){
+            y = 1;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num2.onclick = function(){
+            y = 2;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num3.onclick = function(){
+            y = 3;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num4.onclick = function(){
+            y = 4;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num5.onclick = function(){
+            y = 5;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num6.onclick = function(){
+            y = 6;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num7.onclick = function(){
+            y = 7;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num8.onclick = function(){
+            y = 8;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num9.onclick = function(){
+            y = 9;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+   
+    }
+}
+    function n2(){
+        x = 2;
+mult.onclick = function(){
+        num1.onclick = function(){
+            y = 1;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num2.onclick = function(){
+            y = 2;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num3.onclick = function(){
+            y = 3;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num4.onclick = function(){
+            y = 4;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num5.onclick = function(){
+            y = 5;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num6.onclick = function(){
+            y = 6;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num7.onclick = function(){
+            y = 7;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num8.onclick = function(){
+            y = 8;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num9.onclick = function(){
+            y = 9;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+   
+    }
+}
+    function n3(){
+        x = 3;
+mult.onclick = function(){
+        num1.onclick = function(){
+            y = 1;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num2.onclick = function(){
+            y = 2;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num3.onclick = function(){
+            y = 3;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num4.onclick = function(){
+            y = 4;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num5.onclick = function(){
+            y = 5;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num6.onclick = function(){
+            y = 6;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num7.onclick = function(){
+            y = 7;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num8.onclick = function(){
+            y = 8;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num9.onclick = function(){
+            y = 9;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+   
+    }
+}
+    function n4(){
+        x = 4;
+mult.onclick = function(){
+        num1.onclick = function(){
+            y = 1;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num2.onclick = function(){
+            y = 2;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num3.onclick = function(){
+            y = 3;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num4.onclick = function(){
+            y = 4;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num5.onclick = function(){
+            y = 5;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num6.onclick = function(){
+            y = 6;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num7.onclick = function(){
+            y = 7;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num8.onclick = function(){
+            y = 8;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num9.onclick = function(){
+            y = 9;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+   
+    }
+}
+    function n5(){
+        x = 5;
+mult.onclick = function(){
+        num1.onclick = function(){
+            y = 1;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num2.onclick = function(){
+            y = 2;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num3.onclick = function(){
+            y = 3;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num4.onclick = function(){
+            y = 4;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num5.onclick = function(){
+            y = 5;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num6.onclick = function(){
+            y = 6;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num7.onclick = function(){
+            y = 7;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num8.onclick = function(){
+            y = 8;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num9.onclick = function(){
+            y = 9;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+   
+    }
+}
+    function n6(){
+        x = 6;
+mult.onclick = function(){
+        num1.onclick = function(){
+            y = 1;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num2.onclick = function(){
+            y = 2;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num3.onclick = function(){
+            y = 3;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num4.onclick = function(){
+            y = 4;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num5.onclick = function(){
+            y = 5;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num6.onclick = function(){
+            y = 6;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num7.onclick = function(){
+            y = 7;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num8.onclick = function(){
+            y = 8;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num9.onclick = function(){
+            y = 9;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+   
+    }
+}
+    function n7(){
+        x = 7;
+mult.onclick = function(){
+        num1.onclick = function(){
+            y = 1;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num2.onclick = function(){
+            y = 2;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num3.onclick = function(){
+            y = 3;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num4.onclick = function(){
+            y = 4;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num5.onclick = function(){
+            y = 5;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num6.onclick = function(){
+            y = 6;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num7.onclick = function(){
+            y = 7;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num8.onclick = function(){
+            y = 8;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num9.onclick = function(){
+            y = 9;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+   
+    }
+}
+    function n8(){
+        x = 8;
+mult.onclick = function(){
+        num1.onclick = function(){
+            y = 1;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num2.onclick = function(){
+            y = 2;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num3.onclick = function(){
+            y = 3;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num4.onclick = function(){
+            y = 4;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num5.onclick = function(){
+            y = 5;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num6.onclick = function(){
+            y = 6;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num7.onclick = function(){
+            y = 7;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num8.onclick = function(){
+            y = 8;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num9.onclick = function(){
+            y = 9;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+   
+    }
+}
+    function n9(){
+        x = 9;
+mult.onclick = function(){
+        num1.onclick = function(){
+            y = 1;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num2.onclick = function(){
+            y = 2;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num3.onclick = function(){
+            y = 3;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num4.onclick = function(){
+            y = 4;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num5.onclick = function(){
+            y = 5;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num6.onclick = function(){
+            y = 6;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num7.onclick = function(){
+            y = 7;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num8.onclick = function(){
+            y = 8;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+        num9.onclick = function(){
+            y = 9;
+            ans.textContent = `${x} ✖️ ${y} = ${x * y}`;
+            return;
+        }
+   
+    }
+}
+    rest.onclick = function(){
+        location.reload();
+    }
